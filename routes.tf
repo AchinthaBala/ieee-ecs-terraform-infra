c4d29c95d545fea92f15067f3a8e7f4653c7e747
sbha4 <shabeeha.now@gmail.com>
2026-08-26 15:45:03 +0530

    Add NAT gateway, internet gateway, and route tables

---
+++ routes.tf
@@ -0,0 +1,35 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.ieee_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ieee_igw.id
+  }
+
+  tags = {
+    Name = "my-route-table-pub"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet" {
+  subnet_id      = aws_subnet.pub-sub-1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.ieee_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ieee_nat.id
+  }
+
+  tags = {
+    Name = "my-route-table-pvt"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet" {
+  subnet_id      = aws_subnet.pvt-sub-1.id
+  route_table_id = aws_route_table.private.id
+}
